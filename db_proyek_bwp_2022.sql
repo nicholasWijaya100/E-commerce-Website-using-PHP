3d6fb3b73f24b88057ff68c8ca1aa130c5d34508
nicholasWijaya100 <nich.wijaya.100@gmail.com>
2022-11-22 08:19:41 +0700

    menambah file sql untuk database

--- db_proyek_bwp_2022.sql
+++ db_proyek_bwp_2022.sql
@@ -1,12 +1,43 @@
-CREATE DATABASE /*!32312 IF NOT EXISTS*/'db_proyek_bwp_2022' /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS db_proyek_bwp_2022;
+USE db_proyek_bwp_2022;
 
-USE 'T7_221116980';
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `menu`;
+DROP TABLE IF EXISTS `dtrans`;
+DROP TABLE IF EXISTS `htrans`;
 
-DROP TABLE IF EXISTS 'user';
+CREATE TABLE `users` (
+  `user_id` INT(10) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `saldo` INT(10) NOT NULL,
+  PRIMARY KEY (`users_id`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE 'user' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'usename' varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `menu` (
+  `menu_id` INT(10) NOT NULL AUTO_INCREMENT,
+  `kategori_id` INT(10) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `price` INT(10) NOT NULL,
+  `stok` INT(6) NOT NULL,
+  `image` TEXT NOT NULL,
+  PRIMARY KEY (`menu_id`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `dtrans` (
+  `dtrans_id` INT(10) NOT NULL AUTO_INCREMENT,
+  `menu_id` INT(10) NOT NULL,
+  `quantity` INT(5) NOT NULL,
+  `subtotal` INT(10) NOT NULL,
+  PRIMARY KEY (`dtrans_id`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `htrans` (
+  `htrans_id` INT(10) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(10) NOT NULL,
+  `promo_id` INT(10) NOT NULL,
+  `tanggal_transaksi` DATE NOT NULL,
+  `total` INT(10) NOT NULL,
+  `status` INT(1) NOT NULL,
+  PRIMARY KEY (`htrans_id`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
