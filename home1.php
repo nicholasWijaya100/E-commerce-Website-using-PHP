cb6b65dde01aabb48a02173970cf01fbb7166b1a
timothyjohan <timothyjohan11@gmail.com>
2022-11-25 00:54:25 +0700

    Add Asset and update home1
    Home1 itu home yg ada cssnya

---
+++ home1.php
@@ -0,0 +1,106 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="left-navbar">
+    
+            <div class="nav-items-selected">
+                <div class="items-icon">
+                    <div class="icon-home-selected"></div>
+                </div>
+            </div>
+            
+            <div class="nav-items">
+                <div class="items-icon">
+                    <div class="icon-wallet"></div>
+                </div>
+            </div>
+            
+            <div class="nav-items">
+                <div class="items-icon">
+                    <div class="icon-trans"></div>
+                </div>
+            </div>
+    
+    
+    
+            <div class="nav-items-logout">
+                <div class="items-icon">
+                    <div class="icon-logout"></div>
+                </div>
+            </div>
+    
+                
+            
+        </div>
+    
+        <div class="mid-container">
+            <div class="top">
+                <div class="title"> <p><b>Today's Menu . . .</b></p> </div>
+                <div class="searchbar-container">
+                    <input class="searchbar" type="text" name="search-text" id="">
+                    <div class="search-icon"></div>
+                </div>
+
+            </div>
+
+            <div class="banner">
+                <div class="welcome-text">
+                    <h2>Welcome, Timothy Johan</h2><br>
+                    <h4>Please check our latest offers</h4>
+                </div>
+            </div>
+            <h2 style="margin-left: 5%; margin-top: 25px;">Menu Category</h2>
+            <div class="category-row">
+                <div class="category-card">
+                    <div class="category-icon"></div>
+                    <div class="category-name"><b>Category 1</b></div>
+                    <div class="category-go"> > </div>
+                </div>
+
+                <div class="category-card">
+                    <div class="category-icon"></div>
+                    <div class="category-name"><b>Category 2</b></div>
+                    <div class="category-go"> > </div>
+                </div>
+
+                <div class="category-card">
+                    <div class="category-icon"></div>
+                    <div class="category-name"><b>Category 3</b></div>
+                    <div class="category-go"> > </div>
+                </div>
+
+                <div class="category-card">
+                    <div class="category-icon"></div>
+                    <div class="category-name"><b>Category 4</b></div>
+                    <div class="category-go"> > </div>
+                </div>
+
+                <div class="category-card">
+                    <div class="category-icon"></div>
+                    <div class="category-name"><b>Category 5</b></div>
+                    <div class="category-go"> > </div>
+                </div>
+                <div class="category-card">
+                    <div class="category-icon"></div>
+                    <div class="category-name"><b>Category 6</b></div>
+                    <div class="category-go"> > </div>
+                </div>
+
+            </div>
+            
+            <div class="items-row"></div>
+
+        </div>
+
+        <div class="right-container"></div>
+    </div>
+</body>
+</html>
