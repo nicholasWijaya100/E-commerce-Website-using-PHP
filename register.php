5b46e278d1e541e8f66a2934ed039dbf36d05d47
timothyjohan <timothyjohan11@gmail.com>
2022-11-28 23:34:35 +0700

    Message update
    Registered successful message

--- register.php
+++ register.php
@@ -1,26 +1,35 @@
 <?php
     include("koneksi.php");
 
+    $complete = true;
+    $conf = true;
+    $exist = true;
+    $success = false;
+
     if(isset($_POST['registerBtn'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
         $cpassword = $_POST['cpassword'];
-
+        
         if($username == "" || $password == "" || $cpassword == "") {
             $_SESSION["message"] = "Mohon isi semua field";
+            $complete = false;
         } else {
             if($password != $cpassword) {
                 $_SESSION["message"] = "Password dan confirm password tidak sama";
+                $conf = false;
             } else {
                 $stmt = $conn->query("select * from users where username = '$username'");
                 $res = $stmt->fetch_all(MYSQLI_BOTH);
                 if(count($res) > 0) {
                     $_SESSION["message"] = "Username sudah dipakai, silahkan pilih username lain";
+                    $exist = true;
                 } else {
                     $stmt = $conn->prepare("insert into users(username, password) values(?, ?)");
                     $stmt->bind_param("ss", $username, $password);
                     $stmt->execute();
                     $_SESSION["message"] = "Register Berhasil";
+                    $success = true;
                 }   
             }
         }
@@ -51,7 +60,13 @@
             <input type="password" name="cpassword" placeholder="Masukan Confirm Password" class= "text-input">
             </div>
             
-            
+            <?php 
+                if(!$complete) echo '<div class="message-box">Mohon isi semua field</div>';
+                elseif(!$conf) echo '<div class="message-box">Password dan confirm password tidak sama</div>';
+                elseif(!$exist) echo '<div class="message-box">Username sudah dipakai, silahkan pilih username lain</div>';
+                elseif($success) echo '<div class="message-box inversed-message">Registered Success</div>';
+
+            ?>
            
             
             <input type="submit" value="Login" formaction="index.php" class="login-buttons">
